16f8d5f7ce5a7f4a4239b4ed91e6c9159e1701b1
robinson-arpi <robinson.arpi@gmail.com>
2023-07-09 11:37:13 -0500

    ya vale

--- app.py
+++ app.py
@@ -1,69 +1,14 @@
 from flask import Flask, render_template, request, session
 from flask_socketio import SocketIO, emit, join_room
-import pika
 import threading
-from pika.exceptions import AMQPConnectionError
+from rabbitmq import create_queue, send_message, delete_queue, receive_message
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 # Configuración de RabbitMQ
-RABBITMQ_HOST = 'localhost'
-RABBITMQ_PORT = 5672
-RABBITMQ_USERNAME = 'guest'
-RABBITMQ_PASSWORD = 'guest'
-
-# Crear un diccionario para almacenar los hilos activos por cliente
-active_threads = {}
-
-def consume_messages(queue_name, sender_id, stop_event):
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=RABBITMQ_HOST, port=RABBITMQ_PORT, credentials=pika.PlainCredentials(RABBITMQ_USERNAME, RABBITMQ_PASSWORD))
-    )
-    channel = connection.channel()
-    channel.queue_declare(queue=queue_name)
-
-    def callback(ch, method, properties, body):
-        # Emitir el mensaje recibido al cliente a través de WebSocket
-        if sender_id == 'robin':
-            socketio.emit('message_r', {'message': body.decode()})
-            # Confirmar el procesamiento exitoso del mensaje
-            channel.basic_ack(delivery_tag=method.delivery_tag)
-            print('robin')
-        elif sender_id == 'mauro':
-            socketio.emit('message_m', {'message': body.decode()})
-            # Confirmar el procesamiento exitoso del mensaje
-            channel.basic_ack(delivery_tag=method.delivery_tag)
-            print('mauro')
-        elif sender_id == 'cristian':
-            socketio.emit('message_c', {'message': body.decode()})
-            # Confirmar el procesamiento exitoso del mensaje
-            channel.basic_ack(delivery_tag=method.delivery_tag)
-            print('cristian')
-        elif sender_id == 'vero':
-            socketio.emit('message_v', {'message': body.decode()})    
-            # Confirmar el procesamiento exitoso del mensaje
-            channel.basic_ack(delivery_tag=method.delivery_tag)
-            print('vero')
-        # Imprimir el mensaje consumido en la consola
-        print(f'Message consumed-> {queue_name}: {body.decode()}')
-
-    channel.basic_consume(queue=queue_name, on_message_callback=callback)
-    channel.start_consuming()
-
-def publish_message(queue_name, message):
-    print(f'Received message from client {queue_name}: {message}')
-    try:
-        connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=RABBITMQ_HOST, port=RABBITMQ_PORT, credentials=pika.PlainCredentials(RABBITMQ_USERNAME, RABBITMQ_PASSWORD))
-        )
-        channel = connection.channel()
-        channel.queue_declare(queue=queue_name)
-        channel.basic_publish(exchange='', routing_key=queue_name, body=message.encode())
-        connection.close()
-    except AMQPConnectionError as e:
-        print(f"Error al conectar con RabbitMQ: {e}")
+# ...
 
 @app.route('/')
 def login():
@@ -71,53 +16,56 @@ def login():
 
 @app.route('/chat', methods=['POST'])
 def chat():
-    session['sender_id'] = request.form['sender_id']
     session['recipient_id'] = request.form['recipient_id']
-    return render_template('chat.html', emisor=session['sender_id'], receptor=session['recipient_id'])
+    return render_template('chat.html', receptor=session['recipient_id'])
 
 @socketio.on('connect')
 def handle_connect():
     recipient_id = session.get('recipient_id')
-    sender_id = session.get('sender_id')
 
-    if recipient_id and sender_id:
-        queue_name = f'{recipient_id}_{sender_id}'
-        print(f'consume de -> {queue_name}')
+    if recipient_id:
+        queue_name = f'{recipient_id}'
 
-        # Crear un objeto Event para controlar el ciclo del hilo
-        stop_event = threading.Event()
+        # Crear la cola en RabbitMQ
+        create_queue(queue_name)
 
-        t = threading.Thread(target=consume_messages, args=(queue_name, sender_id, stop_event))
-        t.start()
+        # Crear un objeto Room para el chat
+        join_room(queue_name)
 
-        # Registrar el hilo activo en el diccionario
-        active_threads[sender_id] = (t, stop_event)
+        # Recuperar y enviar el historial de mensajes al nuevo cliente
+        previous_messages = []
+        while True:
+            message = receive_message(queue_name)
+            if message:
+                previous_messages.append(message)
+            else:
+                break
+
+        for message in previous_messages:
+            emit('message', {'message': message, 'queue_name': queue_name})
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    sender_id = session.get('sender_id')
+    recipient_id = session.get('recipient_id')
 
-    if sender_id in active_threads:
-        # Obtener el hilo y el objeto Event correspondientes al cliente desconectado
-        t, stop_event = active_threads.pop(sender_id)
+    if recipient_id:
+        queue_name = f'{recipient_id}'
 
-        # Establecer la bandera en el objeto Event para indicar al hilo que debe finalizar
-        stop_event.set()
-
-        # Esperar a que el hilo termine su ejecución
-        t.join()
-        print("Mata hilo")
+        # Eliminar la cola en RabbitMQ
+        #delete_queue(queue_name)
 
 @socketio.on('message')
-def handle_message(message):
+def handle_message(data):
     recipient_id = session.get('recipient_id')
-    sender_id = session.get('sender_id')
 
-    if recipient_id and sender_id:
-        queue_name = f'{sender_id}_{recipient_id}'
+    if recipient_id:
+        queue_name = f'{recipient_id}'
 
-        # Publicar el mensaje en la cola RabbitMQ
-        publish_message(queue_name, message)
+        # Enviar el mensaje a la cola en RabbitMQ
+        send_message(queue_name, data['message'])
+
+        # Publicar el mensaje en la sala del chat
+        emit('message', {'message': data['message'], 'queue_name': queue_name}, room=queue_name)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
--- rabbitmq.py
+++ rabbitmq.py
@@ -1,17 +1,59 @@
 import pika
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+parameters = pika.ConnectionParameters('localhost')
+connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
 
+def connect():
+    global connection, channel
+    parameters = pika.ConnectionParameters('localhost')
+    while True:
+        try:
+            connection = pika.BlockingConnection(parameters)
+            channel = connection.channel()
+            break
+        except pika.exceptions.AMQPConnectionError:
+            print("Error de conexión. Intentando reconectar...")
+            continue
+
 def create_queue(queue_name):
-    channel.queue_declare(queue=queue_name)
+    try:
+        channel.queue_declare(queue=queue_name, durable=True)
+    except (pika.exceptions.AMQPConnectionError, pika.exceptions.ChannelClosedByBroker):
+        print("Error de conexión. Reconectando...")
+        connect()
+        channel.queue_declare(queue=queue_name, durable=True)
 
 def send_message(queue_name, message):
-    channel.basic_publish(exchange='', routing_key=queue_name, body=message)
+    try:
+        channel.basic_publish(exchange='', routing_key=queue_name, body=message)
+    except (pika.exceptions.AMQPConnectionError, pika.exceptions.ChannelClosedByBroker):
+        print("Error de conexión. Reconectando...")
+        connect()
+        channel.basic_publish(exchange='', routing_key=queue_name, body=message)
 
 def receive_message(queue_name):
-    method_frame, _, body = channel.basic_get(queue=queue_name, auto_ack=True)
-    if method_frame:
-        return body.decode()
-    else:
-        return None
+    try:
+        method_frame, _, body = channel.basic_get(queue=queue_name, auto_ack=True)
+        if method_frame:
+            return body.decode()
+        else:
+            return None
+    except (pika.exceptions.AMQPConnectionError, pika.exceptions.ChannelClosedByBroker):
+        print("Error de conexión. Reconectando...")
+        connect()
+        method_frame, _, body = channel.basic_get(queue=queue_name, auto_ack=True)
+        if method_frame:
+            return body.decode()
+        else:
+            return None
+
+def delete_queue(queue_name):
+    try:
+        channel.queue_delete(queue=queue_name)
+    except (pika.exceptions.AMQPConnectionError, pika.exceptions.ChannelClosedByBroker):
+        print("Error de conexión. Reconectando...")
+        connect()
+        channel.queue_delete(queue=queue_name)
+
+# Llamamos a la función connect para establecer la conexión inicial
+connect()
